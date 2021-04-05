7eb4c452cbb401f580eb4dd0eba3259ad2098a0e
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-04-05 13:36:34 +0200

    Creating the Quote component

--- portfolio_v2_frontend/src/App.js
+++ portfolio_v2_frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import mockPage from './utils/page'
+import Quote from './components/Quote'
 
 function App() {
 
   console.log('App.render mockPage', mockPage);
   return (
     <div className="App">
-      This is it
-
+      <Quote data={mockPage.content[0]}/>
     </div>
   );
 }
